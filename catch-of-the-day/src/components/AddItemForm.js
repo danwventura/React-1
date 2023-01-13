37a0c5d78c453fe21394c8dc8acbf08217a11dee
Ventura <daniel.w.ventura@vumc.org>
2023-01-12 19:23:07 -0600

    Build AddItem form and adding new item to state

---
+++ catch-of-the-day/src/components/AddItemForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class AddItemForm extends React.Component {
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+    statusRef = React.createRef();
+    descRef = React.createRef();
+    imageRef = React.createRef();
+    
+    createItem = (event) => {
+        event.preventDefault();
+        const item = {
+            nameRef: this.nameRef.current.value,
+            priceRef: parseFloat(this.priceRef.current.value), //1054 instead of 10.54
+            statusRef: this.statusRef.current.value,
+            descRef: this.descRef.current.value,
+            imageRef:this.imageRef.current.value
+        }
+        this.props.addItem(item);
+        //refresh the form
+        event.currentTarget.reset();
+    }
+    
+    render(){
+        return (
+            <form className="fish-edit" onSubmit={this.createItem}>
+                <input name="name" ref={this.nameRef} type="text" placeholder="Name"/>
+                <input name="price" ref={this.priceRef} type="text" placeholder="Price"/>
+                <select name="status" ref={this.statusRef}>
+                    <option>Fresh!</option>
+                    <option>Sold Out!</option>
+                </select>
+                <textarea name="desc" ref={this.descRef} placeholder="Desc"/>
+                <input name="image" ref={this.imageRef} type="text" placeholder="Image"/>
+                <button type="submit">+ Add Item </button>
+            </form>
+        )
+    }
+}
+
+export default AddItemForm;
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddItemForm from './AddItemForm';
 
 class Inventory extends React.Component {
     render(){
         return (
-                <div className='inventory'>Inventory!</div>
+                <div className='inventory'>
+                    <h2>Inventory</h2>
+                    <AddItemForm addItem={this.props.addItem}/>
+                </div>
         )
     }
 }
