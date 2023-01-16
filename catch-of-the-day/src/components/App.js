29c7b8024f08f432c4b8d4ec3d463cb3aeacbc1d
Ventura <daniel.w.ventura@vumc.org>
2023-01-15 23:31:43 -0600

    Persisting order state within localstorage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,15 +14,25 @@ class App extends React.Component {
 
     componentDidMount(){
         const { params } = this.props.match;
+        //first reinstate our localStorage
+        const localStorageRef = localStorage.getItem(params.storeId);
+        if(localStorageRef){
+            this.setState({order: JSON.parse(localStorageRef)});
+        }
         this.ref = base.syncState(`${params.storeId}/items`, {
             context: this,
             state: 'items'
         });
     }
 
+    componentDidUpdate(){
+        localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order))
+    }
+
     componentWillUnmount(){
         base.removeBinding(this.ref);
     }
+
 
     addItem = (item) => {
         //1. Take a copy of existing state
