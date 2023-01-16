e88c63e86f30f5ed029504eb0e7c25106a603dcc
Ventura <daniel.w.ventura@vumc.org>
2023-01-16 00:08:29 -0600

    Building Edit Item and updating state from user input

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import AddItemForm from './AddItemForm';
+import EdItemForm from './AddItemForm';
+import EditItemForm from './EditItemForm';
 
 class Inventory extends React.Component {
     render(){
         return (
                 <div className='inventory'>
                     <h2>Inventory</h2>
+                    {Object.keys(this.props.items).map(key => <EditItemForm key={key} index={key} item={this.props.items[key]} updateItem={this.props.updateItem}/>)}
                     <AddItemForm addItem={this.props.addItem}/>
                     <button onClick={this.props.loadSampleItems}>Load Sample Items</button>
                 </div>
